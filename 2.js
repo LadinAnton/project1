3bae3a6230a427c870bbded2c86507b370751738
LadinAnton <64701035+LadinAnton@users.noreply.github.com>
2020-07-15 18:12:41 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,4 @@
+
+var str = '123456789';
+result = str.split('').reverse().join('');
+console.log(result);
---
+++ 3.js
@@ -0,0 +1,7 @@
+
+var str = 'http://ngtualekseeva';
+if (str.substr(0, 7) == 'http://') {
+	console.log('Да');
+} else {
+	console.log('Нет');
+}
