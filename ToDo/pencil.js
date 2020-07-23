45949461622c05258b4b73ab59ce33bd0257ce38
LadinAnton <64701035+LadinAnton@users.noreply.github.com>
2020-07-23 17:37:52 +0300

    Delete pencil.js

--- ToDo/pencil.js
+++
@@ -1,6 +0,0 @@
-function pencil(){
-    document.querySelector("#pencil").addEventListener("click", function(){
-        document.querySelector("input[type='text']").classList.toggle("display");
-    });
-}
-export {pencil};
