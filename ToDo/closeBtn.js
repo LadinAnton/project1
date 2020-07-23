6f5b25719353700c17c1abb1fd8cd99eaa460ae0
LadinAnton <64701035+LadinAnton@users.noreply.github.com>
2020-07-23 17:38:20 +0300

    Delete closeBtn.js

--- ToDo/closeBtn.js
+++
@@ -1,7 +0,0 @@
-function closeBtn() {
-    document.querySelector(".closebtn").addEventListener("click", function() {
-    event.preventDefault();
-    overlay.style.height = "0";
-});
-}
-export {closeBtn};
