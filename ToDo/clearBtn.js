51e312714adc0dd136b954951f2ffa67014a97ed
LadinAnton <64701035+LadinAnton@users.noreply.github.com>
2020-07-23 17:38:28 +0300

    Delete clearBtn.js

--- ToDo/clearBtn.js
+++
@@ -1,6 +0,0 @@
-function clearBtn(){
-    document.querySelector(".clear").addEventListener("click", function(){
-        document.querySelector("ul").innerHTML = "";
-    });
-}
-export {clearBtn};
