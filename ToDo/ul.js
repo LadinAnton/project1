78cf62af36333701c5af8d2693c8221a9092fa5a
LadinAnton <64701035+LadinAnton@users.noreply.github.com>
2020-07-23 17:37:20 +0300

    Delete ul.js

--- ToDo/ul.js
+++
@@ -1,8 +0,0 @@
-function ul() {
-    document.querySelector("ul").addEventListener("click", function (e) {
-        if (e.target.tagName === "LI") {
-            e.target.classList.toggle("checked")
-        }
-    })
-}
-export {ul};
